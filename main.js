aa785b42c47ba9accb8f5a3e5284a30f364b89bb
gaurishukla60 <75981006+gaurishukla60@users.noreply.github.com>
2021-04-19 16:24:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
-function preload(){
+function preload() {
 
 }
 
-function setup() {
-     canvas = createCanvas(640 , 480);
-     canvas.position(110 , 250);
-     video = createCapture(VIDEO);
-     video.hide();
-     tint_color="";
-  }
-  
-  function draw() {
-    image(video,190,150,240,180);
-     circle(50,50,60);
-    fill(160,0,0);
-    circle(320,50,60);
-    fill(160,0,0);
-    circle(50,430,60);
-    fill(160,0,0);
-    circle(50,230,60);
-    fill(160,0,0);
-    circle(590,50,60);
-    fill(160,0,0);
-    circle(590,230,60);
-    fill(160,0,0);
-    circle(590,430,60);
-    fill(160,0,0);
-    circle(320,430,60);
-    fill(160,0,0);
-  }
- function take_snapshot()
- {
-    save('gauri.png');
- }
-  
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+    console.log('poseNet is initialized');
+
+}
+function draw(){
+image(video,0,0,300,300);
+}
+function gotPoses(results)
+    {
+console.log(results);
+console.log("nose x= "+ results[0].pose.nose.x);
+console.log("nose y= "+ results[0].pose.nose.y);
+
+    }
+function take_snapshot(){
+    save('myfilterimage.png');
+}
+
